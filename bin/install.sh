f49efe2705be6304d4b29d2705269bd6c915b25b
concept10 <tenthconcept@gmail.com>
2023-02-19 18:52:43 -0700

    add main remove webpack action

--- bin/install.sh
+++
@@ -1,8 +0,0 @@
-# #!/bin/bash
-
-# set -e
-
-npm install canvas
-npm install --build-from-source canvas
-
-sudo apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
